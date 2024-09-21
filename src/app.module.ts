fe4cecd7c7baf69fd7ec1830571bcd46441a1cc0
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-09-21 15:38:30 +1000

    focusbearwith-backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { FriendModule } from './friend/friend.module';
 import { InviteModule } from './invite/invite.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, InviteModule, FriendModule],
+  imports: [PrismaModule, InviteModule, FriendModule, UserModule],
 })
 export class AppModule {}
